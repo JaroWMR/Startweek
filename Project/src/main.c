28ad7da79e6cc379f24d53c5a1852341ddac1989
Siempie30 <siemvermeulen@hotmail.com>
2024-03-10 17:46:28 +0100

    Removed junk comments, added simple selfmade pwm function

--- Project/src/main.c
+++ Project/src/main.c
@@ -19,10 +19,6 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 #define THREAD0_PRIORITY 7
 #define THREAD1_PRIORITY 7
 
-int brightness = 0;
-int fadeAmount = 5;
-int fadeDelay = 30;
-
 void thread0(void) // printing thread
 {
 	int cnt = 0;
@@ -35,24 +31,6 @@ void thread0(void) // printing thread
 
 void thread1(void) // led thread
 {
-	// int cnt = 0;
-	// while (1) {
-	// 	printk("Hello, I am thread1\t%d\n", cnt);
-	// 	k_msleep(3000);
-	// 	cnt++;
-	// }
-
-	// for (int i = 0; i < 255; i+= fadeAmount) {
-	// 	//setBrightness(i);
-	// 	gpio_pin_toggle_dt(&led);
-	// 	printk("LED!\n");
-	// 	k_msleep(500);
-	// }
-
-	// for (int i = 255; i < 0; i-= fadeAmount) {
-	// 	setBrightness(i);
-	// 	k_msleep(fadeDelay);
-	// }
 	int ret;
 	bool led_state = true;
 
@@ -65,30 +43,33 @@ void thread1(void) // led thread
 		return 0;
 	}
 
+	pwm(8,0);
+
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
+		//ret = gpio_pin_toggle_dt(&led);
+		ret = gpio_pin_set_dt(&led, led_state);
 		
 		if (ret < 0) {
 			return 0;
 		}
 
-		led_state = !led_state;
 		printf("LED state: %s\n", led_state ? "ON" : "OFF");
+		led_state = !led_state;
 		k_msleep(SLEEP_TIME_MS);
 	}
 }
 
-// void setBrightness(int brightness) {
-// 	float dutyCycle = brightness / 255.0;
+void pwm(int brightness, int duration) {
+	// brightness may be a value from 0 (off) to 10 (max brightness)
+	
+	for (int i = 0; i < 200; i++) {
+		gpio_pin_set_dt(&led, 1);
+		k_msleep(brightness);
+		gpio_pin_set_dt(&led, 0);
+		k_msleep(10-brightness);
+	}
 
-// 	int onTime = dutyCycle * fadeDelay;
-
-// 	gpio_pin_toggle_dt(&led);
-// 	k_msleep(fadeDelay);
-
-// 	gpio_pin_toggle_dt(&led);
-// 	k_msleep(fadeDelay-onTime);
-// }
+}
 
 // Define the threads
 K_THREAD_DEFINE(thread0_id, STACKSIZE, thread0, NULL, NULL, NULL, THREAD0_PRIORITY, 0, 0);
