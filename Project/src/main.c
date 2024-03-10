c5e2b70f05ef9f4b48b81a9b7a94693bf010f37a
Siempie30 <siemvermeulen@hotmail.com>
2024-03-10 19:41:04 +0100

    LED functionality now changes based on button press

--- Project/src/main.c
+++ Project/src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/sys/util.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/drivers/gpio.h>
 
@@ -11,6 +13,16 @@
 #define LED0_NODE DT_ALIAS(led0)
 
 /*
+ * Get button configuration from the devicetree sw0 alias. This is mandatory.
+ */
+#define SW0_NODE	DT_ALIAS(sw0)
+#if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
+static struct gpio_callback button_cb_data;
+
+/*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
@@ -20,6 +32,11 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 #define TLED_PRIORITY 7
 
 int ledFunction = 1; //0=fade, 1=step
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+}
 
 void pwm(int brightness, int duration) {
 	// brightness may be a value from 0 (off) to 10 (max brightness)
@@ -54,9 +71,30 @@ void ledStep() {
 
 void tcore(void) // Core thread
 {
+	int ret;
+
+	if (!gpio_is_ready_dt(&button)) {
+		printk("Error: button device %s is not ready\n", button.port->name);
+	}
+
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n", ret, button.port->name, button.pin);
+	}
+
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n", ret, button.port->name, button.pin);
+	}
+
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
+
 	int cnt = 0;
 	while (1) {
 		printk("Hello, I am thread0\t%d\n", cnt);
+		ledFunction = gpio_pin_get_dt(&button);
 		k_msleep(540);
 		cnt++;
 	}
