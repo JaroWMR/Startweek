6717fbcfa56d831a0c5fb13ed384573313f86bd1
Siempie30 <siemvermeulen@hotmail.com>
2024-03-12 14:21:31 +0100

    Made runRests script universally usable
    - Script is no longer user dependent
    - TAKE NOTE: the environment variable 'STARTWEEK_ROOT' must be set to the root of the git repo for this to work
    - Script automatically executes the tests and stores results in the corresponding dir.
    - Known issue: when running the tests multiple times, a new dir is automatically created (which is not ignored by git)

--- Project/tests/runTests.sh
+++ Project/tests/runTests.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-PROJECT_ROOT="/home/siem/startweek/zephyrproject/Startweek"
-echo "$PROJECT_ROOT/Project/tests/" 
-exec $PROJECT_ROOT/../zephyr/scripts/twister -O $PROJECT_ROOT/Project/tests/results -T $PROJECT_ROOT/Project/tests/
+exec $STARTWEEK_ROOT/../zephyr/scripts/twister -O $STARTWEEK_ROOT/Project/tests/results -T $STARTWEEK_ROOT/Project/tests/
