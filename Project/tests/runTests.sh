90aaf1ff76500358a91cad9236fd347ac6910997
Siempie30 <siemvermeulen@hotmail.com>
2024-03-12 14:28:05 +0100

    Fixed automatic new output dirs
    There's no longer new output directories generated by twister. Fixed by removing the old output dir in the script before executing the new one.

--- Project/tests/runTests.sh
+++ Project/tests/runTests.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+# Remove the result directory if it exists
+rm -rf $STARTWEEK_ROOT/Project/tests/results
 exec $STARTWEEK_ROOT/../zephyr/scripts/twister -O $STARTWEEK_ROOT/Project/tests/results -T $STARTWEEK_ROOT/Project/tests/
