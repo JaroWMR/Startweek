732f7e3e62c02f0a777d805aaf53e22856f918f4
Sean <heavydragon99@outlook.com>
2024-03-25 14:58:28 +0100

    Read sensor data at 40 hertz

--- Project/src/main.c
+++ Project/src/main.c
@@ -9,105 +9,142 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
+#include <math.h>
+
+#define M_PIL 3.141592653589793238462643383279502884L
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 
-// #ifdef CONFIG_LIS3MDL_TRIGGER
-// static int gyro_trig_cnt;
+#ifdef CONFIG_LIS3MDL_TRIGGER
+static int gyro_trig_cnt;
 
-// static void gyro_trigger_handler(const struct device *dev,
-// 									const struct sensor_trigger *trig)
-// {
-// 	sensor_sample_fetch_chan(dev, trig->chan);
-// 	gyro_trig_cnt++;
-// }
-// #endif
-
-// int main(void)
-// {
-// 	struct sensor_value magn_xyz[3];
-// 	static const struct device *gyro = DEVICE_DT_GET_ONE(st_lis3mdl_magn);
-
-// #if defined(CONFIG_LIS3MDL_TRIGGER)
-// 	struct sensor_trigger trig;
-// 	int cnt = 1;
-// #endif
-
-// 	if (!device_is_ready(gyro))
-// 	{
-// 		printf("Could not get LIS3MDL device\n");
-// 		return 0;
-// 	}
-
-// #ifdef CONFIG_LIS3MDL_TRIGGER
-// 	trig.type = SENSOR_TRIG_DATA_READY;
-// 	trig.chan = SENSOR_CHAN_MAGN_XYZ;
-// 	sensor_trigger_set(gyro, &trig, gyro_trigger_handler);
-// #endif
-
-// 	while (1)
-// 	{
-// #ifndef CONFIG_LIS3MDL_TRIGGER
-// 		if (sensor_sample_fetch(gyro) < 0)
-// 		{
-// 			printf("LIS3MDL Sensor sample update error\n");
-// 			return 0;
-// 		}
-// #endif
-
-// 		sensor_channel_get(gyro, SENSOR_CHAN_MAGN_XYZ, magn_xyz);
-
-// 		printf(
-// 			"LIS3MDL: Magnetic field (gauss): x: %.1f, y: %.1f, z: %.1f\n",
-// 			sensor_value_to_double(&magn_xyz[0]),
-// 			sensor_value_to_double(&magn_xyz[1]),
-// 			sensor_value_to_double(&magn_xyz[2]));
-
-// #if defined(CONFIG_LIS3MDL_TRIGGER)
-// 		printk("%d:: lis3mdl trig %d\n", cnt, gyro_trig_cnt);
-// 		cnt++;
-// #endif
-
-// 		k_sleep(K_MSEC(100)); // Sleep for 1 second
-// 	}
-
-// 	return 1;
-// }
+static void gyro_trigger_handler(const struct device *dev,
+								 const struct sensor_trigger *trig)
+{
+	sensor_sample_fetch_chan(dev, trig->chan);
+	gyro_trig_cnt++;
+}
+#endif
 
 int main(void)
 {
-    static const struct device *gyro = DEVICE_DT_GET_ONE(st_lis3mdl_magn);
+	struct sensor_value magn_xyz[3];
+	double magn_xyz_double[3];
+	static const struct device *gyro = DEVICE_DT_GET_ONE(st_lis3mdl_magn);
 
-    if (!device_is_ready(gyro)) {
-        printk("Could not get LIS3MDL device\n");
-        return 0;
-    }
+#if defined(CONFIG_LIS3MDL_TRIGGER)
+	struct sensor_trigger trig;
+	int cnt = 1;
+#endif
 
-    struct sensor_value x_axis, y_axis, z_axis;
+	if (!device_is_ready(gyro))
+	{
+		printf("Could not get LIS3MDL device\n");
+		return 0;
+	}
 
-    while (1) {
-        if (sensor_sample_fetch(gyro) < 0) {
-            printk("Sensor sample fetch failed\n");
-            return 0;
-        }
+#ifdef CONFIG_LIS3MDL_TRIGGER
+	trig.type = SENSOR_TRIG_DATA_READY;
+	trig.chan = SENSOR_CHAN_MAGN_XYZ;
+	sensor_trigger_set(gyro, &trig, gyro_trigger_handler);
+#endif
 
-        if (sensor_channel_get(gyro, SENSOR_CHAN_MAGN_X, &x_axis) < 0 ||
-            sensor_channel_get(gyro, SENSOR_CHAN_MAGN_Y, &y_axis) < 0 ||
-            sensor_channel_get(gyro, SENSOR_CHAN_MAGN_Z, &z_axis) < 0) {
-            printk("Could not read sensor channels\n");
-            return 0;
-        }
+	while (1)
+	{
+#ifndef CONFIG_LIS3MDL_TRIGGER
+		if (sensor_sample_fetch(gyro) < 0)
+		{
+			printf("LIS3MDL Sensor sample update error\n");
+			return 0;
+		}
+#endif
 
-        printk("X: %f, Y: %f, Z: %f\n",
-               sensor_value_to_double(&x_axis),
-               sensor_value_to_double(&y_axis),
-               sensor_value_to_double(&z_axis));
+		sensor_channel_get(gyro, SENSOR_CHAN_MAGN_XYZ, magn_xyz);
 
-        k_sleep(K_MSEC(100)); // Sleep for 1 second
-    }
+		for (int i = 0; i < 3; i++)
+		{
+			magn_xyz_double[i] = sensor_value_to_double(&magn_xyz[i]);
+		}
+
+		double direction;
+		if (magn_xyz_double[0] == 0)
+		{
+			if (magn_xyz_double[1] < 0)
+			{
+				direction = 90;
+			}
+			else
+			{
+				direction = 0;
+			}
+		}
+		else
+		{
+			//https://arduino.stackexchange.com/questions/18625/converting-three-axis-magnetometer-values-to-degrees
+			direction = atan2(magn_xyz_double[0], magn_xyz_double[1]) * 180 / M_PIL;
+		}
+
+		if (direction < 0)
+		{
+			direction = direction + 360;
+		}
+		else if (direction > 360)
+		{
+			direction = direction - 360;
+		}
+
+		printf(
+			"LIS3MDL: Magnetic field (gauss): x: %.1f, y: %.1f, z: %.1f\n",
+			magn_xyz_double[0],
+			magn_xyz_double[1],
+			magn_xyz_double[2]);
+		printf("Direction = %.1f degrees\n", direction);
+
+#if defined(CONFIG_LIS3MDL_TRIGGER)
+		printk("%d:: lis3mdl trig %d\n\n", cnt, gyro_trig_cnt);
+		cnt++;
+#endif
+
+		k_sleep(K_MSEC(100)); // Sleep for 1 second
+	}
 
 	return 1;
 }
+
+// int main(void)
+// {
+//     static const struct device *gyro = DEVICE_DT_GET_ONE(st_lis3mdl_magn);
+
+//     if (!device_is_ready(gyro)) {
+//         printk("Could not get LIS3MDL device\n");
+//         return 0;
+//     }
+
+//     struct sensor_value x_axis, y_axis, z_axis;
+
+//     while (1) {
+//         if (sensor_sample_fetch(gyro) < 0) {
+//             printk("Sensor sample fetch failed\n");
+//             return 0;
+//         }
+
+//         if (sensor_channel_get(gyro, SENSOR_CHAN_MAGN_X, &x_axis) < 0 ||
+//             sensor_channel_get(gyro, SENSOR_CHAN_MAGN_Y, &y_axis) < 0 ||
+//             sensor_channel_get(gyro, SENSOR_CHAN_MAGN_Z, &z_axis) < 0) {
+//             printk("Could not read sensor channels\n");
+//             return 0;
+//         }
+
+//         printk("X: %f, Y: %f, Z: %f\n",
+//                sensor_value_to_double(&x_axis),
+//                sensor_value_to_double(&y_axis),
+//                sensor_value_to_double(&z_axis));
+
+//         k_sleep(K_MSEC(100)); // Sleep for 1 second
+//     }
+
+// 	return 1;
+// }
