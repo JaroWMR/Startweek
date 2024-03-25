09584b75f39b4f2706a5516c1770505df7dae935
Sean <heavydragon99@outlook.com>
2024-03-25 09:19:50 +0100

    Not working Gyroscope

--- Project/src/main.c
+++ Project/src/main.c
@@ -10,25 +10,17 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
-
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-
-//static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
     static const struct device *gyro = DEVICE_DT_GET(DT_NODELABEL(gyro));
 
     if (!device_is_ready(gyro)) {
-        printk("Could not get LIS3MDL device\n");
+        printf("Could not get LIS3MDL device\n");
         return 0;
     }
 
@@ -36,18 +28,18 @@ int main(void)
 
     while (1) {
         if (sensor_sample_fetch(gyro) < 0) {
-            printk("Sensor sample fetch failed\n");
+            printf("Sensor sample fetch failed\n");
             return 0;
         }
 
         if (sensor_channel_get(gyro, SENSOR_CHAN_MAGN_X, &x_axis) < 0 ||
             sensor_channel_get(gyro, SENSOR_CHAN_MAGN_Y, &y_axis) < 0 ||
             sensor_channel_get(gyro, SENSOR_CHAN_MAGN_Z, &z_axis) < 0) {
-            printk("Could not read sensor channels\n");
+            printf("Could not read sensor channels\n");
             return 0;
         }
 
-        printk("X: %f, Y: %f, Z: %f\n",
+        printf("X: %f, Y: %f, Z: %f\n",
                sensor_value_to_double(&x_axis),
                sensor_value_to_double(&y_axis),
                sensor_value_to_double(&z_axis));
