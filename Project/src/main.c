319ac3291f25e49da16f52a3780d4a93f739351d
Sean <heavydragon99@outlook.com>
2024-03-25 10:19:11 +0100

    Copied code from x_nucleo_iks01a1 example

--- Project/src/main.c
+++ Project/src/main.c
@@ -15,37 +15,64 @@
  * See the sample documentation for information on how to fix this.
  */
 
+#ifdef CONFIG_LIS3MDL_TRIGGER
+static int gyro_trig_cnt;
+
+static void gyro_trigger_handler(const struct device *dev,
+									const struct sensor_trigger *trig)
+{
+	sensor_sample_fetch_chan(dev, trig->chan);
+	gyro_trig_cnt++;
+}
+#endif
+
 int main(void)
 {
-    static const struct device *gyro = DEVICE_DT_GET(DT_NODELABEL(gyro));
+	struct sensor_value magn_xyz[3];
+	static const struct device *gyro = DEVICE_DT_GET_ONE(st_lis3mdl_magn);
 
-    if (!device_is_ready(gyro)) {
-        printf("Could not get LIS3MDL device\n");
-        return 0;
-    }
+#if defined(CONFIG_LIS3MDL_TRIGGER)
+	struct sensor_trigger trig;
+	int cnt = 1;
+#endif
 
-    struct sensor_value x_axis, y_axis, z_axis;
+	if (!device_is_ready(gyro))
+	{
+		printf("Could not get LIS3MDL device\n");
+		return 0;
+	}
 
-    while (1) {
-        if (sensor_sample_fetch(gyro) < 0) {
-            printf("Sensor sample fetch failed\n");
-            return 0;
-        }
+#ifdef CONFIG_LIS3MDL_TRIGGER
+	trig.type = SENSOR_TRIG_DATA_READY;
+	trig.chan = SENSOR_CHAN_MAGN_XYZ;
+	sensor_trigger_set(gyro, &trig, gyro_trigger_handler);
+#endif
 
-        if (sensor_channel_get(gyro, SENSOR_CHAN_MAGN_X, &x_axis) < 0 ||
-            sensor_channel_get(gyro, SENSOR_CHAN_MAGN_Y, &y_axis) < 0 ||
-            sensor_channel_get(gyro, SENSOR_CHAN_MAGN_Z, &z_axis) < 0) {
-            printf("Could not read sensor channels\n");
-            return 0;
-        }
+	while (1)
+	{
+#ifndef CONFIG_LIS3MDL_TRIGGER
+		if (sensor_sample_fetch(gyro) < 0)
+		{
+			printf("LIS3MDL Sensor sample update error\n");
+			return 0;
+		}
+#endif
 
-        printf("X: %f, Y: %f, Z: %f\n",
-               sensor_value_to_double(&x_axis),
-               sensor_value_to_double(&y_axis),
-               sensor_value_to_double(&z_axis));
+		sensor_channel_get(gyro, SENSOR_CHAN_MAGN_XYZ, magn_xyz);
 
-        k_sleep(K_MSEC(1000)); // Sleep for 1 second
-    }
+		printf(
+			"LIS3MDL: Magnetic field (gauss): x: %.1f, y: %.1f, z: %.1f\n",
+			sensor_value_to_double(&magn_xyz[0]),
+			sensor_value_to_double(&magn_xyz[1]),
+			sensor_value_to_double(&magn_xyz[2]));
+
+#if defined(CONFIG_LIS3MDL_TRIGGER)
+		printk("%d:: lis3mdl trig %d\n", cnt, gyro_trig_cnt);
+		cnt++;
+#endif
+
+		k_sleep(K_MSEC(1000)); // Sleep for 1 second
+	}
 
 	return 1;
 }
