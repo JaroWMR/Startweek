8ba16445b84ded0782498cc250de7c78f4205a61
jaroWMR <jarorutjes07@gmail.com>
2024-04-08 09:01:15 +0200

    begin of POC

--- Project/src/main.c
+++ Project/src/main.c
@@ -7,46 +7,65 @@
 #include <inttypes.h>
 
 #define SLEEP_TIME_MS	1
+#define Switches
+#define Buttons
+#define Matrix
+#define Rotary
 
-
-#define SW0_NODE	DT_ALIAS(d2)
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
+#define d2	DT_ALIAS(d2)
+#define d3	DT_ALIAS(d3)
+#define d4	DT_ALIAS(d4)
+#define d5	DT_ALIAS(d5)
+#define d6	DT_ALIAS(d6)
+#define d7	DT_ALIAS(d7)
+#define d8	DT_ALIAS(d8)
+#define d9	DT_ALIAS(d9)
+#define d10	DT_ALIAS(d10)
+#define d11	DT_ALIAS(d11)
+static const struct gpio_dt_spec d2 = GPIO_DT_SPEC_GET_OR(d2, gpios,
 							      {0});
-static struct gpio_callback button_cb_data;
 
-void button_pressed(const struct device *dev, struct gpio_callback *cb,
-		    uint32_t pins)
+bool temp;
+
+void gpio_init()
 {
-	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+	int ret;
+	if (!gpio_is_ready_dt(&d2)) {
+		printk("Error: button device %s is not ready\n",
+		       d2.port->name);
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&d2, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n",
+		       ret, d2.port->name, d2.pin);
+		return 0;
+	}
 }
+
 
 int main(void)
 {
-	int ret;
+	
 
-	if (!gpio_is_ready_dt(&button)) {
-		printk("Error: button device %s is not ready\n",
-		       button.port->name);
-		return 0;
+	gpio_init();
+	printk("Begin Test\n");
+	
+	while(true)
+	{
+		#ifdef Switches
+			temp = gpio_pin_get(d2.port, d2.pin);
+			printk("sw5 off: %d\n",temp);
+			temp = gpio_pin_get(d2.port, d2.pin);
+			printk("sw5  on: %d\n",temp);
+		#else
+			printf("Macro 'test' is not defined\n");
+		#endif
+		
+
+		k_busy_wait(1000000);
 	}
-
-	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-	if (ret != 0) {
-		printk("Error %d: failed to configure %s pin %d\n",
-		       ret, button.port->name, button.pin);
-		return 0;
-	}
-
-	ret = gpio_pin_interrupt_configure_dt(&button,
-					      GPIO_INT_EDGE_TO_ACTIVE);
-	if (ret != 0) {
-		printk("Error %d: failed to configure interrupt on %s pin %d\n",
-			ret, button.port->name, button.pin);
-		return 0;
-	}
-
-	gpio_add_callback(button.port, &button_cb_data);
-	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
 
 	return 0;
 }
