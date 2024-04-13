778b5a0c665630429ed91724f231b8400dc553ba
jaroWMR <jarorutjes07@gmail.com>
2024-04-13 11:49:18 +0200

    added buttons, switch and matrix.

--- Project/src/main.c
+++ Project/src/main.c
@@ -12,34 +12,43 @@
 #define Matrix
 #define Rotary
 
-//define btn
-static const struct gpio_dt_spec btn1 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r1), gpios,{0});
-static const struct gpio_dt_spec btn2 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r2), gpios,{0});
-static const struct gpio_dt_spec btn3 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r3), gpios,{0});
-static const struct gpio_dt_spec btn4 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r4), gpios,{0});
-static const struct gpio_dt_spec btn5 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r5), gpios,{0});
-static const struct gpio_dt_spec btn6 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r6), gpios,{0});
-static const struct gpio_dt_spec btn7 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r7), gpios,{0});
-static const struct gpio_dt_spec btn8 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r8), gpios,{0});
-static const struct gpio_dt_spec btn9 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r9), gpios,{0});
-static const struct gpio_dt_spec btn10 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r10), gpios,{0});
-static const struct gpio_dt_spec btn11 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r11), gpios,{0});
-static const struct gpio_dt_spec btn12 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r12), gpios,{0});
-static const struct gpio_dt_spec btn13 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r13), gpios,{0});
-static const struct gpio_dt_spec btn14 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r14), gpios,{0});
-static const struct gpio_dt_spec btn15 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r15), gpios,{0});
-static const struct gpio_dt_spec btn16 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r16), gpios,{0});
+
+
+static const struct gpio_dt_spec buttons[16] = {
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r1), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r2), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r3), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r4), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r5), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r6), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r7), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r8), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r9), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r10), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r11), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r12), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r13), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r14), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r15), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r16), gpios, {0})
+};
 //define switches
-static const struct gpio_dt_spec swtichon1 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r17), gpios,{0});
-static const struct gpio_dt_spec swtichoff1 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r18), gpios,{0});
-static const struct gpio_dt_spec swtichon2 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r19), gpios,{0});
-static const struct gpio_dt_spec swtichoff2 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r20), gpios,{0});
-static const struct gpio_dt_spec swtichon3 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r21), gpios,{0});
-static const struct gpio_dt_spec swtichoff3 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r22), gpios,{0});
-static const struct gpio_dt_spec swtichon4 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r23), gpios,{0});
-static const struct gpio_dt_spec swtichoff4 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r24), gpios,{0});
-static const struct gpio_dt_spec swtichon5 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r25), gpios,{0});
-static const struct gpio_dt_spec swtichoff5 = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r26), gpios,{0});
+static const struct gpio_dt_spec switchon[5] = {
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r17), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r19), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r21), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r23), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r25), gpios, {0}),
+};
+
+static const struct gpio_dt_spec switchoff[5] = {
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r18), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r20), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r22), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r24), gpios, {0}),
+    GPIO_DT_SPEC_GET_OR(DT_ALIAS(r26), gpios, {0}),
+};
+
 //define leds
 static const struct gpio_dt_spec MUXa = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r27), gpios,{0});
 static const struct gpio_dt_spec MUXb = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r28), gpios,{0});
@@ -47,28 +56,100 @@ static const struct gpio_dt_spec shiftdatain = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r29)
 static const struct gpio_dt_spec outputenable = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r30), gpios,{0});
 static const struct gpio_dt_spec shiftclock = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r31), gpios,{0});
 //define rotary switch
-static const struct gpio_dt_spec RotarySwitch = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r32), gpios,{0});
-								  
+//static const struct gpio_dt_spec RotarySwitch = GPIO_DT_SPEC_GET_OR(DT_ALIAS(r32), gpios,{0});
 								  
 
-bool temp;
+#define HIGH 1
+#define LOW 0
+
+bool btnState[16] = {false};
+
+bool switchState[5] = {false};
+
+
+bool temp1;
+bool temp2;
 
 void gpio_init()
 {
-	int ret;
-	if (!gpio_is_ready_dt(&d2)) {
-		printk("Error: button device %s is not ready\n",
-		       d2.port->name);
-		return 0;
+	int ret = 0;	
+	for (uint8_t i = 0; i < 16; i++)
+	{
+		ret += gpio_pin_configure_dt(&buttons[i],GPIO_INPUT);
 	}
 
-	ret = gpio_pin_configure_dt(&d2, GPIO_INPUT);
+	for (uint8_t i = 0; i < 5; i++)
+	{
+		ret += gpio_pin_configure_dt(&switchoff[i],GPIO_INPUT);
+	}
+
+	for (uint8_t i = 0; i < 5; i++)
+	{
+		ret += gpio_pin_configure_dt(&switchon[i],GPIO_INPUT);
+	}
+
+
+	ret += gpio_pin_configure_dt(&MUXa, GPIO_OUTPUT_ACTIVE);
+	ret += gpio_pin_configure_dt(&MUXb, GPIO_OUTPUT_ACTIVE);
+	ret += gpio_pin_configure_dt(&shiftdatain, GPIO_OUTPUT_ACTIVE);
+	ret += gpio_pin_configure_dt(&outputenable, GPIO_OUTPUT_ACTIVE);
+	ret += gpio_pin_configure_dt(&shiftclock, GPIO_OUTPUT_ACTIVE);
+
+
 	if (ret != 0) {
-		printk("Error %d: failed to configure %s pin %d\n",
-		       ret, d2.port->name, d2.pin);
-		return 0;
+		printk("Error");
+		while(true){}
 	}
 }
+
+void SendOneBitData(bool ShiftDataValue)
+{
+	gpio_pin_set_dt(&shiftdatain,ShiftDataValue);
+	//k_busy_wait(10000);
+	gpio_pin_set_dt(&shiftclock,HIGH);
+	//k_busy_wait(10000);
+	gpio_pin_set_dt(&shiftclock,LOW);
+}
+
+void LedMatrix()
+{
+	
+
+	int count = 0;
+	
+	for (size_t a = 0; a < 2; a++)
+	{
+		if(count == 4)
+		{
+			count = 0;
+		}
+
+		for (size_t i = 0; i < 2; i++)
+		{
+			if(a == 0)
+			{
+				SendOneBitData(HIGH);
+				SendOneBitData(LOW);
+			}
+			else
+			{
+				SendOneBitData(LOW);
+				SendOneBitData(HIGH);
+			}
+			
+		}
+		
+		gpio_pin_set_dt(&outputenable,HIGH);
+		gpio_pin_set_dt(&outputenable,LOW);
+
+		gpio_pin_set_dt(&MUXa,(count & 0x1));
+		gpio_pin_set_dt(&MUXb,(count & 0x2));
+
+		count++;
+		k_sleep(K_MSEC(1));
+	}
+}
+
 
 
 int main(void)
@@ -76,22 +157,41 @@ int main(void)
 	
 
 	gpio_init();
+	gpio_pin_set_dt(&shiftdatain,LOW);
+	gpio_pin_set_dt(&outputenable,LOW);
+	gpio_pin_set_dt(&shiftclock,LOW);
+	gpio_pin_set_dt(&MUXa,LOW);
+	gpio_pin_set_dt(&MUXb,LOW);
+	k_sleep(K_MSEC(10));
+
+	
 	printk("Begin Test\n");
 	
 	while(true)
 	{
-		#ifdef Switches
-			temp = gpio_pin_get(d2.port, d2.pin);
-			printk("sw5 off: %d\n",temp);
-			temp = gpio_pin_get(d2.port, d2.pin);
-			printk("sw5  on: %d\n",temp);
-		#else
-			printf("Macro 'test' is not defined\n");
-		#endif
-		
+		for (int i = 0; i < 16; i++)
+		{
+			temp1 = gpio_pin_get(buttons[i].port, buttons[i].pin);
+			if (temp1 != btnState[i]) {
+				printk("btn%d: %d\n", i, temp1);
+				btnState[i] = temp1;
+			}
+		}
 
-		k_busy_wait(1000000);
+		for (int i = 0; i < 5; i++)
+		{
+			temp1 = gpio_pin_get(switchon[i].port, switchon[i].pin);
+			temp2 = gpio_pin_get(switchoff[i].port, switchoff[i].pin);
+			if (temp1 != switchState[i]) {
+				printk("btn%d: %d\n", i, temp1);
+				switchState[i] = (bool)temp1;
+			}
+		}
+
+		while (switchState[1] == true && switchState[2] == false && switchState[3] == true && switchState[4] == false && switchState[5] == true)
+		{
+			LedMatrix();
+		}
 	}
-
 	return 0;
 }
