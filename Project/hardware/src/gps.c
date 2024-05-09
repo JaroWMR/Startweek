9fd13e161366a1a81305fa334759bae061c619b3
siempie30 <siemvermeulen@hotmail.com>
2024-05-09 16:17:03 +0200

    Removed unused calculations from gps fix callback

--- Project/hardware/src/gps.c
+++ Project/hardware/src/gps.c
@@ -11,13 +11,6 @@ struct gnss_data gpsData;
 static void gnss_data_cb(const struct device *dev, const struct gnss_data *data)
 {
 	if (data->info.fix_status != GNSS_FIX_STATUS_NO_FIX) {
-		//printf("%s has fix!\r\n", dev->name);
-		int64_t latMilliDeg = data->nav_data.latitude / 1000000;
-		int64_t lonMilliDeg = data->nav_data.longitude / 1000000;
-		//printf("Lat: %lld millideg\n", latMilliDeg);
-		//printf("Lon: %lld millideg\n", lonMilliDeg);
-		//printf("Alt: %d mm\n", data->nav_data.altitude);
-
 		gpsData = *data;
 	}
 }
