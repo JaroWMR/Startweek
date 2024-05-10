3e81aae15c9a3224d6799a1e81d09ae1fd883d5c
siempie30 <siemvermeulen@hotmail.com>
2024-05-10 12:12:15 +0200

    Added locations definitions file, cleaned up distance print

--- Project/states/src/idle.c
+++ Project/states/src/idle.c
@@ -1,5 +1,6 @@
 #include "idle.h"
 #include "gps.h"
+#include "locations.h"
 #include <zephyr/kernel.h>
 #include <stdio.h>
 
@@ -12,12 +13,6 @@ void getIdleThreads(char ***names, unsigned *amount) {
 }
 
 int playIdle() {
-	// Values for testing the GPS:
-	int64_t dbLatNanoDeg = 51687593000;
-	int64_t dbLonNanoDeg = 5286511000;
-	int64_t vnrLatNanoDeg = 51536111110;
-	int64_t vnrLonNanoDeg = 5979444440;
-	
 
 	while(1) {
 		printf("Looping mg1\n");
@@ -29,13 +24,11 @@ int playIdle() {
 		} else {
 			long double currLat = nanoDegToLdDeg(getLatitude());
 			long double currLon = nanoDegToLdDeg(getLongitude());
-			long double vnrLat = nanoDegToLdDeg(vnrLatNanoDeg);
-			long double vnrLon = nanoDegToLdDeg(vnrLonNanoDeg);
+			long double vnrLat = nanoDegToLdDeg(LAT_VNR);
+			long double vnrLon = nanoDegToLdDeg(LON_VNR);
 			long double dist = getDistanceMeters(currLat,currLon,vnrLat, vnrLon);
 
-			printf("Latitude: %lld\n", getLatitude());
-			printf("Longitude: %lld\n", getLongitude());
-			printf("Distance: %Lf\n", dist);
+			printf("Distance (rounded to whole meters): %d\n", (int)round(dist));
 		}
 		k_msleep(1000);
 	}
