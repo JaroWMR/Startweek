7ce418d48605ae3e13f9ff20af00ab6b504fbe31
jaroWMR <jarorutjes07@gmail.com>
2024-05-20 19:05:36 +0200

    included ztest

--- Project/tests/src/main.c
+++ Project/tests/src/main.c
@@ -1,27 +1,13 @@
 #include <zephyr/ztest.h>
 #include "threads.h"
 #include <stdio.h>
+#include "config.h"
 
 void test_main(void)
 {
 	disableAllThreads();
-	printf("Initialization\n");
 
+	ztest_run_all(NULL, false, 1, 1);
 
-    //  struct power_sequence_state state;
-
-    //  /* Only suites that use a predicate checking for phase == PWR_PHASE_0 will run. */
-    //  state.phase = PWR_PHASE_0;
-    //  ztest_run_all(&state, false, 1, 1);
-
-    //  /* Only suites that use a predicate checking for phase == PWR_PHASE_1 will run. */
-    //  state.phase = PWR_PHASE_1;
-    //  ztest_run_all(&state, false, 1, 1);
-
-    //  /* Only suites that use a predicate checking for phase == PWR_PHASE_2 will run. */
-    //  state.phase = PWR_PHASE_2;
-    //  ztest_run_all(&state, false, 1, 1);
-
-    //  /* Check that all the suites in this binary ran at least once. */
-    //  ztest_verify_all_test_suites_ran();
+	ztest_verify_all_test_suites_ran();
 }
