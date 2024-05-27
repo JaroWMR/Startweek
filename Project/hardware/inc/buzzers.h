6fde9b28cc5af1c6ad3b840afdef5edb3fa42ee2
heavydragon99 <heavydragon99@outlook.com>
2024-05-27 16:27:16 +0200

    WIP on Buzzers_driver_h743zi

--- Project/hardware/inc/buzzers.h
+++ Project/hardware/inc/buzzers.h
@@ -1,7 +1,9 @@
 #ifndef BUZZERS_h
 #define BUZZERS_h
 
-void tbuzzers(void);
+uint8_t buzzersInit();
+uint8_t buzzerSetPwm(int aBuzzerNum, int aFreq);
+uint8_t buzzersExit();
 
 
 #endif /* BUZZERS_h */
--- Project/hardware/src/buzzers.c
+++ Project/hardware/src/buzzers.c
@@ -5,7 +5,11 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/pwm.h>
 
-static struct pwm_dt_spec buzzer = PWM_DT_SPEC_GET(DT_ALIAS(buzzer));
+static struct pwm_dt_spec buzzer1 = PWM_DT_SPEC_GET(DT_ALIAS(buzzer1));
+static struct pwm_dt_spec buzzer2 = PWM_DT_SPEC_GET(DT_ALIAS(buzzer2));
+static struct pwm_dt_spec buzzer3 = PWM_DT_SPEC_GET(DT_ALIAS(buzzer3));
+
+static bool isInit = false;
 
 /** Specify PWM period in seconds */
 #define PWM_SEC(x) (PWM_MSEC(x) * 1000UL)
@@ -17,6 +21,37 @@ static struct pwm_dt_spec buzzer = PWM_DT_SPEC_GET(DT_ALIAS(buzzer));
 
 #define period2 PWM_HZ(200)
 #define pulse2 period2 * 0.2
+
+uint8_t buzzersInit()
+{
+	if (!pwm_is_ready_dt(&buzzer1))
+	{
+		printk("Error: PWM device %s is not ready\n",
+			   buzzer1.dev->name);
+		return 1;
+	}
+	if (!pwm_is_ready_dt(&buzzer2))
+	{
+		printk("Error: PWM device %s is not ready\n",
+			   buzzer2.dev->name);
+		return 1;
+	}
+	if (!pwm_is_ready_dt(&buzzer3))
+	{
+		printk("Error: PWM device %s is not ready\n",
+			   buzzer3.dev->name);
+		return 1;
+	}
+
+	return 0;
+}
+
+uint8_t buzzerSetPwm(int aBuzzerNum, int aFreq){
+	if(aBuzzerNum < 1 || aBuzzerNum > 3){
+		printf("Incorrect buzzer number\n");
+		return 1;
+	}
+}
 
 void tbuzzers(void) // Buzzers thread
 {
@@ -45,17 +80,17 @@ void tbuzzers(void) // Buzzers thread
 			printk("channel = %i\n", buzzer.channel);
 			printk("error while seting is %i\n", pwm_set_dt(&buzzer, period1, pulse1));
 			k_sleep(K_SECONDS(4));
-			//printk("error while disabling is %i\n", pwm_set_dt(&buzzer, 0, 0));
+			// printk("error while disabling is %i\n", pwm_set_dt(&buzzer, 0, 0));
 
-			//k_sleep(K_SECONDS(1));
+			// k_sleep(K_SECONDS(1));
 
 			buzzer.channel = 4;
 			printk("channel = %i\n", buzzer.channel);
 			printk("error while setting is %i\n", pwm_set_dt(&buzzer, period2, pulse2));
 			k_sleep(K_SECONDS(4));
-			//printk("error while disableing is %i\n", pwm_set_dt(&buzzer, 0, 0));
+			// printk("error while disableing is %i\n", pwm_set_dt(&buzzer, 0, 0));
 
-			//k_sleep(K_SECONDS(1));
+			// k_sleep(K_SECONDS(1));
 		}
 	}
 }
