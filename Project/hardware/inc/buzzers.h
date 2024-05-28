f8eea04d883ddf3ca23bd661c253cf65a1729104
heavydragon99 <heavydragon99@outlook.com>
2024-05-28 15:23:00 +0200

    Buzzer driver code works
    Reformatted code so it is a working driver with doxygen comments

--- Project/hardware/inc/buzzers.h
+++ Project/hardware/inc/buzzers.h
@@ -1,8 +1,10 @@
 #ifndef BUZZERS_h
 #define BUZZERS_h
+#include <stdio.h>
 
 uint8_t buzzersInit();
 uint8_t buzzerSetPwm(int aBuzzerNum, int aFreq);
+uint8_t buzzerTurnOff(int aBuzzerNum);
 uint8_t buzzersExit();
 
 
